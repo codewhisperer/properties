abaec13b113472a26cba2025134941cc52d76933
Michael Langmayr <michael.langmayr@qumu.com>
2016-02-11 16:48:10 +0000

    added front-end build chain with react.js and webpack

---
+++ configs/connection.js
@@ -0,0 +1,56 @@
+// FILE: configs/connection.js
+var mysqlAdapter = require('sails-mysql');
+var sqlite3Adapter = require('waterline-sqlite3');
+var myDiskAdapter = require('sails-disk');
+
+module.exports = {
+
+  // setup adapters for each type of database
+  adapters: {
+    'default': mysqlAdapter,
+    disk: myDiskAdapter,
+    mysql: mysqlAdapter,
+    sqlite3: sqlite3Adapter
+  },
+
+  // setup all type of connections you could have
+  connections: {
+    // mysql connection configs
+    mysqlLocalhost: {
+      adapter: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'properties', // change your database name here
+      user: 'root',
+      password: 'password'
+    },
+    //using sails-disk method
+    diskLocal: {
+        adapter: 'disk'
+    },
+    sqlitedb: {
+      adapter: 'sqlite3',
+      /**
+       * Database instance type. Specify whether to store the database on disk
+       * or in memory.
+       */
+      type: 'disk', // or 'memory'
+
+      /**
+       * Location of file if type='disk'
+       */
+      filename: './db.sqlite',
+
+      /**
+       * Set to true to output SQL queries
+       */
+      debug: false
+    }
+  },
+
+  // some config about migration or something
+  defaults: {
+    // migration mode
+    migrate: 'safe'
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+module.exports = {
+  entry: [
+    './frontend/main.jsx'
+  ],
+  output: {
+    filename: 'frontend.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/,   loader: "jsx-loader?insertPragma=React.DOM&harmony" },
+      { test: /\.json$/,   loader: "json-loader" },
+      { test: /\.js$/,   loader: "uglify" },
+      { test: /\.css$/,    loader: "style-loader!css-loader" },
+      { test: /\.html$/,   loader: "raw" },
+      { test: /\.(png|gif|jpg|eot)$/, loader: "file?name=[path][name].[ext]" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  bail: true,
+  cache: true
+};
